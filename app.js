a827fa3430b1f4eec24c846b210fa9fd6e473c6c
prakash-systango <ppatel@systango.com>
2023-10-24 18:33:01 +0530

    are yar

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Update this as needed
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.get("/user",(req,res)=>{
     res.send("welcome to jungle")
 })
